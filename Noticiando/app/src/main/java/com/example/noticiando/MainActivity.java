f5551620e091e06f6d3bdae0dbfaae38315b3873
menor2025 <joaopedro_jp80@hotmail.com>
2021-08-31 22:51:00 -0300

    Subiu Balão 3

--- Noticiando/app/src/main/java/com/example/noticiando/MainActivity.java
+++ Noticiando/app/src/main/java/com/example/noticiando/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.noticiando;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button botao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botao = findViewById(R.id.botaoprox);
+
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(MainActivity.this, NoticiaDetail.class);
+                startActivity(it);
+            }
+        });
     }
+
+
 }
---
+++ Noticiando/app/src/main/java/com/example/noticiando/NoticiaDetail.java
@@ -0,0 +1,14 @@
+package com.example.noticiando;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NoticiaDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_noticia_detail);
+    }
+}
