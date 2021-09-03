84acb68db5c5e2be6409b0d8fd2924236f0a6005
Victor Cassão <victorcassao@gmail.com>
2021-09-02 22:36:02 -0300

    Finalmente saímos do 0

--- Noticiando/app/src/main/java/com/example/noticiando/MainActivity.java
+++ Noticiando/app/src/main/java/com/example/noticiando/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -16,44 +17,62 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     Button botao;
+    Button botao2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        ListView noticia = findViewById(R.id.lvNoticias);
-//        String[] dados = new String[]{"teste1", "teste1"}
+        ListView listNoticias = (ListView) findViewById(R.id.lista_noticias);
 
-        ListView listaNoticias = (ListView) findViewById(R.id.lvNoticias);
-        ArrayList<String> noticias = preencherDados();
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, noticias);
-        listaNoticias.setAdapter(ArrayAdapter);
+        String[] noticias = new String[]{"Noticia 1", "Noticia 2","Noticia 3"};
 
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,noticias);
 
-//        ArrayAdapter<String> adapter = new ArrayAdapter<String>
-//                (this, android.R.layout.simple_list_item_1,dados);
+        listNoticias.setAdapter(adapter);
 
 
+//        Noticia noticia = new Noticia("Menor dá o rabo",
+//                                    "Menor é pego dando o rabo","O menor foi visto pelos estudantes dando o rabo no banheiro...",
+//                                    "http:teste.com.br");
 
-        botao = findViewById(R.id.botaoprox);
+//        ArrayList<Noticia> listNoticias = null;
+//
+//        listNoticias.add(noticia);
 
-        botao.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent it = new Intent(MainActivity.this, NoticiaDetail.class);
-                startActivity(it);
-            }
-        });
-    }
 
-    private ArrayList<String> preencherDados(){
-        ArrayList<String> dados = new ArrayList<String>();
-        dados.add("Teste1");
-        dados.add("Teste2");
-        dados.add("Teste3");
-        dados.add("Teste4");
-        return dados;
+//        botao = findViewById(R.id.botaoprox);
+//        botao.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                // Mandando pra segunda tela
+//                // Intent it = new Intent(MainActivity.this, NoticiaDetail.class);
+//                Intent it = new Intent(getApplicationContext(),NoticiaDetail.class);
+//                Bundle parametros = new Bundle();
+//                parametros.putString("titulo",noticia.getTittulo());
+//                parametros.putString("descricao",noticia.getDescricao());
+//                it.putExtras(parametros);
+//                startActivity(it);
+//
+//            }
+//        });
+//
+//        botao2 = findViewById(R.id.button2);
+//        botao2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                TextView tela_teste = findViewById(R.id.tela_noticia);
+//
+//                tela_teste.setText(noticia.getTittulo());
+//            }
+//        });
+
+
+
+
+
+
     }
 
 
--- Noticiando/app/src/main/java/com/example/noticiando/NoticiaDetail.java
+++ Noticiando/app/src/main/java/com/example/noticiando/NoticiaDetail.java
@@ -2,7 +2,9 @@ package com.example.noticiando;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class NoticiaDetail extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class NoticiaDetail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_noticia_detail);
+
+        Intent recebendo_info = getIntent();
+
+        Bundle parametros = recebendo_info.getExtras();
+
+        TextView titulo_noticia_tv = findViewById(R.id.titulo_noticia);
+        TextView descricao_noticia_tv = findViewById(R.id.descricao_noticia);
+
+        titulo_noticia_tv.setText(parametros.getString("titulo"));
+        descricao_noticia_tv.setText(parametros.getString("descricao"));
+
+
     }
 }
