2cdad608ad3503467c944fb14488c02dc84abffb
Victor Cassão <victorcassao@gmail.com>
2021-09-05 13:05:43 -0300

    opção com json

--- Noticiando/app/src/main/java/com/example/noticiando/MainActivity.java
+++ Noticiando/app/src/main/java/com/example/noticiando/MainActivity.java
@@ -5,16 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public void testando_json(String list_json) throws JSONException {
+        JSONObject my_obj = new JSONObject(list_json);
+
+        String content = my_obj.getString("author");
+
+        Toast.makeText(getApplicationContext(),content, Toast.LENGTH_LONG).show();
+    }
+
+    String json_string = "{'author': 'Josh Partlow',\n" +
+            "   'content': 'He asked me two or three times, Abdullah recalled. We were back and forth on the telephone. And then he [asked] if the two of us could be together? I said, Thats a good idea. \\r\\nStory continues below … [+9129 chars]',\n" +
+            "   'description': '',\n" +
+            "   'publishedAt': '2021-09-03T00:31:57Z',\n" +
+            "   'source': {'id': 'the-washington-post', 'name': 'The Washington Post'},\n" +
+            "   'title': 'Hamid Karzai is back in the thick of Afghan politics but a long way from power. - The Washington Post',\n" +
+            "   'url': 'https://www.washingtonpost.com/world/2021/09/02/afghanistan-karzai-taliban-government-power/',\n" +
+            "   'urlToImage': 'https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/7T2CYYA3OUI6VF32CWTHCDWW3I.jpg&w=1440'}";
+
 
     Button botao;
     Button botao2;
@@ -24,13 +47,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         ListView listNoticias = (ListView) findViewById(R.id.lista_noticias);
-
         String[] noticias = new String[]{"Noticia 1", "Noticia 2","Noticia 3"};
-
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,noticias);
 
+
+
         listNoticias.setAdapter(adapter);
+
+        // criar ação para evento de click na notícia
+        listNoticias.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+//                Toast.makeText(getApplicationContext(),
+//                        "Clicou no item " + position,
+//                        Toast.LENGTH_LONG).show();
+
+                try {
+                    testando_json(json_string);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+
+
 
 
 //        Noticia noticia = new Noticia("Menor dá o rabo",
