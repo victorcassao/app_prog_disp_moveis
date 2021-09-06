06d9093ea03ebbe11fdc9d3ab8590a1383688c45
Victor Cassão <victorcassao@gmail.com>
2021-09-06 14:33:34 -0300

    removido variável que tinha a string json

--- Noticiando/app/src/main/java/com/example/noticiando/MainActivity.java
+++ Noticiando/app/src/main/java/com/example/noticiando/MainActivity.java
@@ -7,11 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
-
 import java.util.ArrayList;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -34,45 +31,8 @@ public class MainActivity extends AppCompatActivity {
         return listaNoticias;
     }
 
-    String json_string_noticias = "{\n" +
-            "    'articles': \n" +
-            "    [\n" +
-            "        {\n" +
-            "            'author': None,\n" +
-            "            'content': 'Parents struggling to balance work and homeschooling say moving the school holidays forward would take the pressure off.\\r\\n National wants the Government to look seriously into moving holidays earlier… [+3974 chars]',\n" +
-            "            'description': 'Parents struggling to balance work and homeschooling say moving the school holidays forward would make their lives easier.',\n" +
-            "            'publishedAt': '2021-09-03T00:33:00Z',\n" +
-            "            'source': {'id': None, 'name': 'Stuff.co.nz'},\n" +
-            "            'title': 'Moving school holidays would take pressure off lockdown juggle, parents say - Stuff.co.nz',\n" +
-            "            'url': 'https://www.stuff.co.nz/national/education/300397721/moving-school-holidays-would-take-pressure-off-lockdown-juggle-parents-say',\n" +
-            "            'urlToImage': 'https://resources.stuff.co.nz/content/dam/images/4/y/u/l/0/d/image.related.StuffLandscapeSixteenByNine.1420x800.4yukd5.png/1630629197182.jpg'\n" +
-            "        },\n" +
-            "        {\n" +
-            "            'author': 'Josh Partlow',\n" +
-            "            'content': 'He asked me two or three times, Abdullah recalled. We were back and forth on the telephone. And then he [asked] if the two of us could be together? I said, Thats a good idea. \\r\\nStory continues below … [+9129 chars]',\n" +
-            "            'description': 'Oh my good',\n" +
-            "            'publishedAt': '2021-09-03T00:31:57Z',\n" +
-            "            'source': {'id': 'the-washington-post', 'name': 'The Washington Post'},\n" +
-            "            'title': 'Hamid Karzai is back in the thick of Afghan politics but a long way from power. - The Washington Post',\n" +
-            "            'url': 'https://www.washingtonpost.com/world/2021/09/02/afghanistan-karzai-taliban-government-power/',\n" +
-            "            'urlToImage': 'https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/7T2CYYA3OUI6VF32CWTHCDWW3I.jpg&w=1440'\n" +
-            "        },\n" +
-            "        {\n" +
-            "            'author': 'Eurosport',\n" +
-            "            'content': 'England players celebrate, Hungary v England, World Cup qualifying, Puskas Arena, Budapest, September 2, 2021\\r\\nImage credit: Getty Images',\n" +
-            "            'description': 'England comfortably beat Hungary 4-0 in Budapest, on an evening overshadowed by abhorrent behaviour from a section of the home fans.',\n" +
-            "            'publishedAt': '2021-09-02T23:25:00Z',\n" +
-            "            'source': {'id': None, 'name': 'Eurosport.com AU'},\n" +
-            "            'title': 'Sterling sparks England win in hostile atmosphere to see off Hungary - Eurosport COM',\n" +
-            "            'url': 'https://www.eurosport.com/football/world-cup-qualification-uefa/2022/football-news-hungary-v-england-follow-live-coverage_sto8511678/story.shtml',\n" +
-            "            'urlToImage': 'https://i.eurosport.com/2021/09/02/3212006-65795388-2560-1440.jpg'\n" +
-            "        }\n" +
-            "    ]\n" +
-            "}";
-
     ArrayList<Noticia> listaNoticias = new ArrayList<Noticia>();
     APINewsHelper apiNewsHelper = new APINewsHelper();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -117,9 +77,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
     };
-
-
 
 };
